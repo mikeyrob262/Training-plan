5ca2d36847772b905a97f14909ef1a2af1ea89f0
mikeyrob262 <mgrobinson07@gmail.com>
2026-07-09 17:31:52 -0400

    Bump cache buster + rings

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 // build pipeline verification - 2026-07-02
 export default {
   async fetch(request, env, ctx) {
-    return new Response(`<!DOCTYPE html><!-- BUST1783630016 v1783630016 -->
+    return new Response(`<!DOCTYPE html><!-- BUST1783632562 v1783632562 -->
 <html lang="en">
 <head>
 <meta charset="UTF-8"><!-- 1783629145 -->
